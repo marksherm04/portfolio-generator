2d3b062617e5b1f688043284e955dd217a28a7bc
Mark S <marksherm04@gmail.com>
2021-06-02 13:24:34 -0400

    added about me section and option to choose y/n

--- app.js
+++ app.js
@@ -6,17 +6,46 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: githubInput => {
+                if (githubInput) {
+                   return true;
+                }  else {
+                   console.log("Please enter your Github Username!");
+                   return false
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -37,12 +66,28 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your project?'
+                message: 'What is the name of your project?',
+                validate: projectName => {
+                    if (projectName) {
+                       return true;
+                    }  else {
+                       console.log("Please enter your project name!");
+                       return false
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required)'
+                message: 'Provide a description of the project (Required)',
+                validate: projectDescription => {
+                    if (projectDescription) {
+                       return true;
+                    }  else {
+                       console.log("Please enter your project description!");
+                       return false
+                    }
+                }
             },
             {
                 type: 'checkbox',
@@ -53,7 +98,15 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)'
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: projectLink => {
+                    if (projectLink) {
+                       return true;
+                    }  else {
+                       console.log("Please enter your Github project link!");
+                       return false
+                    }
+                }
             },
             {
                 type: 'confirm',
