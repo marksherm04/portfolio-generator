316c3c48107ce3b122cc5b7dfbedc710a4b7a27a
Mark S <marksherm04@gmail.com>
2021-06-03 15:01:38 -0400

    cleaned up code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
@@ -139,40 +139,19 @@ const promptUser = () => {
     promptUser()
         .then(promptProject)
         .then(portfolioData => {
-            const pageHTML = generatePage(portfolioData);
-
-            fs.writeFile('./index.html', pageHTML, err => {
-                if (err) throw new Error(err);
-
-                console.log('Page created! Check out index.html in this directory to see it!');
+           return generatePage(portfolioData);
+            })
+            .then(pageHTML => {
+              return writeFile(pageHTML);
+            })
+            .then(writeFileResponse => {
+              console.log(writeFileResponse);
+              return copyFile();
+            })
+            .then(copyFileResponse => {
+              console.log(copyFileResponse);
+            })
+            .catch(err => {
+              console.log(err);
             });
-        });
-    
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//     }
-
-
-// console.log('===========');
-
-// // Is the same as this...
-// profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem)
-//     });
-// };      
 
