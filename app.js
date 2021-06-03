70bcbc73fc429e42b7b5913402eeba1d4b09a576
Mark S <marksherm04@gmail.com>
2021-06-03 13:29:53 -0400

    added sections and refactored code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
  return inquirer
@@ -49,6 +51,9 @@ const promptUser = () => {
         }
     ]);
 };
+
+
+  
 
 
     const promptProject = portfolioData => {
@@ -134,22 +139,20 @@ const promptUser = () => {
     promptUser()
         .then(promptProject)
         .then(portfolioData => {
-            console.log(portfolioData);
+            const pageHTML = generatePage(portfolioData);
+
+            fs.writeFile('./index.html', pageHTML, err => {
+                if (err) throw new Error(err);
+
+                console.log('Page created! Check out index.html in this directory to see it!');
+            });
         });
     
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
 
 
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw (err);
 
-//     console.log('Portfolio complete!  Check out index.html to see the output!');
-// });
 
 
 
